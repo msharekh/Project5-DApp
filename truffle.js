8fdc51ac2f1459ae03adc03dd9a722e4d4cb21b1
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-01-20 23:56:39 +0300

    before deploy to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,4 +1,6 @@
 // Allows us to use ES6 in our migrations and tests.
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
 require('babel-register');
 
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
@@ -10,6 +12,17 @@ module.exports = {
       host: '127.0.0.1',
       port: 9545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          'uphold picnic age aunt sheriff embody dune gauge merit hero purse innocent',
+          'https://rinkeby.infura.io/v3/141bd5d2a2d243a7aab8388795ffd2ea'
+        );
+      },
+      network_id: '4',
+      gas: 4500000,
+      gasPrice: 10000000000
     }
   }
 };
